671c35a8206080b7e8f1cd7a0b938cb4e6fdc3d3
Kemalettin Aktaş <kemalettin.aktas@gazi.edu.tr>
2019-12-19 10:45:36 +0300

    Proje dosyası ekle.

---
+++ DelegateCihazKontrolApp/Cihaz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCihazKontrolApp
+{
+    class Cihaz
+    {
+        private delegate void TemsilcimHandler();
+        TemsilcimHandler faaliyet;
+        public Cihaz()
+        {
+            faaliyet = new TemsilcimHandler(Ac);
+                }
+        private void Ac()
+        {
+
+            Console.WriteLine("cihaz açıldı");
+            faaliyet = Kapat;
+        }
+        private void Kapat()
+        { Console.WriteLine("cihaz kapandı");
+            faaliyet = Ac;
+        }
+        public void Kontrol()
+        { faaliyet(); }
+    }
+}
---
+++ DelegateCihazKontrolApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCihazKontrolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cihaz chz = new Cihaz();
+            for (int i = 0; i < 7; i++)
+            {
+                chz.Kontrol();
+            }
+            Console.ReadKey();
+        }
+    }
+}
